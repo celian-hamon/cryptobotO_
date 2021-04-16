d454ec9596ea08189a611b16da493da9779c6c77
skelletondude <skelletondude@gmail.com>
2021-04-16 18:22:12 +0200

    fix
    fixed

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from pycoingecko import CoinGeckoAPI
 from random import randint
 from licto import lictoo,emoj
 from merge import get_img_lead
+from PIL import Image
 import tweepy
 
 consumer_key = 'bgwMY7ETrRQtUJXQW50a4HFsr'
@@ -46,7 +47,12 @@ def leaderboard(cg):
     troisieme_crypto = str(market[2]['id'])
     quatrieme_crypto = str(market[3]['id'])
     cinquieme_crypto = str(market[4]['id'])
-    get_img_lead(c1=premiere_crypto,c2=deuxieme_crypto,c3=troisieme_crypto,c4=quatrieme_crypto,c5=cinquieme_crypto)
+    s1 = str(market[0]['symbol'])
+    s2 = str(market[1]['symbol'])
+    s3 = str(market[2]['symbol'])
+    s4 = str(market[3]['symbol'])
+    s5 = str(market[4]['symbol'])
+    get_img_lead(c1=s1,c2=s2,c3=s3,c4=s4,c5=s5)
     tweet_lead = 'curent leaderbord of crypto :\n1rst #' + premiere_crypto + '\n2nd #' + deuxieme_crypto + '\n3rd #' + troisieme_crypto + '\n4st #' + quatrieme_crypto + '\n5st #' + cinquieme_crypto + "\n💰💰💰💰💰" 
     print(tweet_lead)
     return tweet_lead
@@ -55,14 +61,15 @@ def Tweet(api,tweet=0,image=0,id_RT=0):
     if image == 0 :
         last_tweet = api.update_status(status=tweet)
         return last_tweet
-    if image !=0 :
-        last_tweet = api.update_status(status=tweet,media_ids=[])
+    if image != 0 :
+        image_path = "images/"+image+".jpg"
+        last_tweet = api.update_with_media(image_path,tweet)
     if id_RT != 0 :
         last_retweet = api.retweet(id=id_RT)
         return last_retweet
 
-# while True:
-#     Tweet(api,tweet=leaderboard(cg))
-#     for i in range(72):
-#         Tweet(api,tweet=crypto_weather(cg))
-#         sleep(3600)
+while True:
+    for i in range(72):
+        Tweet(api,tweet=crypto_weather(cg),image="btc_eth")
+        sleep(3600)
+    Tweet(api,tweet=leaderboard(cg),image="merged")
