80a8d5e8cc4c413390f302416c7f6bb4a9d9711a
skelletondude <skelletondude@gmail.com>
2021-04-17 18:16:28 +0200

    finished image merger (merge.py)
    need some fix and change

--- merge.py
+++ merge.py
@@ -6,15 +6,26 @@ def get_img_lead(c1,c2,c3,c4,c5):
         remove("images/merged.png")
     except :
         pass
-    c1= str("images/color/"+c1+".png")
+    c1 = str("images/color/"+c1+".png")
     c2 = str("images/color/"+c2+".png")
+    c3 = str("images/color/"+c3+".png")
+    c4 = str("images/color/"+c4+".png")
+    c5 = str("images/color/"+c5+".png")
     img_crypto1 = Image.open(c1)
     img_crypto2 = Image.open(c2)
+    img_crypto3 = Image.open(c3)
+    img_crypto4 = Image.open(c4)
+    img_crypto5 = Image.open(c5)
     img_crypto1_size = img_crypto1.size
     img_crypto2_size = img_crypto2.size
-    new_image = Image.new('RGB',(2*img_crypto2_size[0], img_crypto1_size[1]), (250,250,250))
+    new_image = Image.new('RGB',(5*img_crypto2_size[0], img_crypto1_size[1]), (250,250,250))
     new_image.paste(img_crypto1,(0,0))
-    new_image.paste(img_crypto2,(img_crypto1_size[0],0))
+    new_image.paste(img_crypto2,(128,0))
+    new_image.paste(img_crypto3,(256,0))
+    new_image.paste(img_crypto4,(384,0))
+    new_image.paste(img_crypto5,(512,0))
     new_image.save("images/merged.png","png")
     return 
+
+get_img_lead("doge","eth","btc",'html',"xrp")
 
