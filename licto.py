a8b51fd5003db7d379995bad46cd8a6082015b8f
skelletondude <skelletondude@gmail.com>
2021-04-17 17:53:01 +0200

    Update licto.py

--- licto.py
+++ licto.py
@@ -1,7 +1,16 @@
 from random import randint
 
 lictoo = [
-    'have a nice day'
+    'have a nice day',
+    'be happy in your life',
+    'i hope your day will be good',
+    'you are a good person',
+    'hope your family is fine',
+    'hope you are getting rich',
+    'good evening or good morning ;)',
+    'dont hesitate to give btc ;P',
+    'dont hesitate to dm or comment or follow',
+    'follow to dont miss my report or other post :)'
 ]
 
 emoj = [
